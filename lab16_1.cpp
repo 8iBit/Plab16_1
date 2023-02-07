0e2dcd645dab4e088ba033d208349ae17409f788
porrapat <iporrapat@gmail.com>
2023-02-07 15:13:11 +0700

    lab16_1.cpp

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,27 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+	int a = 5;
+	string b = "A";
+	string &c = b;
+	int *x = &a;
+	string *y = &b;
+	int *z = &*x;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z;
 
-	
-	
+	cout << "\n" << &a << " " << &b << " " << &c << " " << &x << " " << &y << " " << &z;
+
+	c="F";
+	cout << "\n" << a << " " << b << " " << c << " " << x << " " << y << " " << z;
+
+	*y="W";
+	cout << "\n" << a << " " << b << " " << c << " " << x << " " << y << " " << z;
+
+	*x=6;
+	cout << "\n" << a << " " << b << " " << c << " " << x << " " << y << " " << z;
+
+	*z=7;
+	cout << "\n" << a << " " << b << " " << c << " " << x << " " << y << " " << z;
 	return 0;
 }
